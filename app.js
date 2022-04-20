5e86be9a51ed76a4c7b004e32f7fb667b0d80481
Sebastian Salazar <sebastiansalazar@Sebastians-MacBook-Air.local>
2022-04-20 14:10:21 -0400

    more consolelogs

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('test')
+
+function addNumber( a, b){
+
+    return a + b
+}
+
+console.log(addNumber(4,8))
+console.log(addNumber(4,8))
+console.log(addNumber(4,8))
+console.log(addNumber(4,8))
